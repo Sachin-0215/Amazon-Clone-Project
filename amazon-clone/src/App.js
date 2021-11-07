54a9c7ddfb24b1ed53660e37058077fdfe1c3039
Sachin Gupta <sgupta15120@gmail.com>
2021-11-07 20:19:08 +0530

    Modification

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./Header";
-import Home from "./Home";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
 export default App;
